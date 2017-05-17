1b8619840b2f6c6495de3b9f48337c3e4e01a365
diasantoso <mikhaelmds@gmail.com>
2017-05-17 22:38:03 +0700

    Last Commit

--- cdry.sql
+++ cdry.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2017 at 02:30 PM
+-- Generation Time: May 11, 2017 at 10:55 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -23,29 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
 CREATE TABLE `orders` (
   `id_order` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `weight` float NOT NULL,
-  `price` float NOT NULL,
-  `date_order` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `weight` float DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `date_order` timestamp NULL DEFAULT NULL,
   `date_end` timestamp NULL DEFAULT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Undone'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `orders`
 --
 
 INSERT INTO `orders` (`id_order`, `email`, `address`, `weight`, `price`, `date_order`, `date_end`, `status`) VALUES
-(21, 'dias@mail.com', 'Jalan Magelang', 5, 5000, '2017-03-21 10:35:16', '2017-03-23 10:35:16', 'Done'),
-(22, 'samuel@mail.com', 'Jalan Janti', 10, 10000, '2017-03-22 10:35:16', '2017-03-24 10:35:16', 'Done'),
-(23, 'aldi@mail.com', 'Jalan Solo Raya', 20, 20000, '2017-03-24 10:35:16', '2017-03-29 10:35:16', 'Done'),
-(24, 'david@mail.com', 'Jalan Babarsari Raya', 3, 3000, '2017-03-21 10:35:16', '2017-03-22 10:35:16', 'Done');
+(21, 'dias@mail.com', 'Jalan Magelang', 5, 5000, '2017-03-20 17:00:00', '2017-03-22 17:00:00', 'Done'),
+(22, 'samuel@mail.com', 'Jalan Janti', 10, 10000, '2017-03-21 17:00:00', '2017-03-23 17:00:00', 'Done'),
+(23, 'aldi@mail.com', 'Jalan Solo Raya', 0, 0, '2017-04-27 17:00:00', '2017-04-27 17:00:00', 'Undone'),
+(24, 'david@mail.com', 'Jalan Babarsari Raya', 3, 3000, '2017-03-20 17:00:00', '2017-03-21 17:00:00', 'Done'),
+(27, 'david@mail.com', 'Jalan Jalan', 0, 0, NULL, NULL, 'Undone'),
+(28, 'david@mail.com', 'Jalan Jalan', NULL, NULL, '2017-03-20 17:00:00', NULL, 'Undone');
 
 -- --------------------------------------------------------
 
@@ -67,18 +69,19 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `email`, `password`, `name`, `address`, `role`) VALUES
-(21, 'dias@mail.com', 'dias', 'Mikhael Diastama Santoso', 'Jalan Babarsari Raya no 120', 0),
+(21, 'dias@mail.com', 'dias', 'aaaa', 'aaa', 1),
 (22, 'samuel@mail.com', 'samuel', 'Samuel Jan Lukito', 'Jalan Solo Raya no 69', 0),
 (23, 'aldi@mail.com', 'aldi', 'Ignatius Aldi Pradana', 'Jalan Janti no 888', 0),
 (24, 'david@mail.com', 'david', 'Samuel David Sutanto', 'Jalan Magelang no 999', 0),
-(25, 'admin', 'admin', 'Admin C-DRY', '-', 1);
+(28, 'demon@mail.com', 'demon', 'Demonstrasi', 'Jalan Pancasila', 0),
+(29, '1@mail.com', '1', '1', 'Jalan Pancasila', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `order`
+-- Indexes for table `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id_order`);
@@ -94,15 +97,15 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `order`
+-- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
